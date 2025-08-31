5945c7358158f27f8add5cadaabdd485cd639300
Rahul Rudra <rahulrudra665@gmail.com>
2025-08-31 10:59:41 +0600

    changes made

---
+++ UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/How Js Work Behind Scenes/script.js
@@ -0,0 +1,37 @@
+"use strict";
+const name1 = "Rahul";
+// const name1 = "Rahul-1";
+function calcAge(birthYear) {
+	const age = 2037 - birthYear;
+	console.log(firstName);
+	const name1 = "Rahul-1";
+
+	console.log(name1);
+	function printAge() {
+		const output = `Ypur are ${age} born in ${birthYear}`;
+		console.log(output);
+		console.log("Age", age);
+		console.log(firstName);
+	}
+	printAge();
+
+	if (age > 20) {
+		const millenium = true;
+
+		function add(a, b) {
+			return a + b;
+		}
+		// var millenium = true;
+		add(1, 2);
+	}
+
+	// console.log(millenium);
+	// console.log(lastName);
+
+	return age;
+}
+
+const firstName = "Jonas";
+// calcAge(1991);
+console.log(calcAge(1991));
+// console.log(age);
