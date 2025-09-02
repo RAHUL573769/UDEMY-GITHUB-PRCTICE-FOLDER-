febd988c7ad3c2c0b41d70d230824f0d7ebb23b7
Rahul Rudra <rahulrudra665@gmail.com>
2025-09-02 08:13:48 +0600

    changes made

---
+++ UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/How Js Work Behind Scenes/script1.js
@@ -0,0 +1,34 @@
+// console.log(name2);
+
+// console.log(name1);
+
+// console.log(name3);
+"use strict";
+let name1 = "Rahul";
+var name2 = "Rahul-1";
+const name3 = "Rahul-5";
+
+//Functions
+
+// console.log("13", addNum(1, 2));
+// console.log("14", addExpr(23, 56));
+// console.log("15", addExpr11(34));
+// console.log("16", addExpr2(3, 4));
+
+// console.log(addArrow(3, 5));
+function addNum(a, b) {
+	return a + b;
+}
+
+const addExpr = function (a, n) {};
+var addExpr11 = function (a, n) {};
+let addExpr2 = function (a, n) {};
+var addArrow = (a, b) => a + b;
+
+//Examples
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+function deleteShoppingCart() {
+	console.log("All Products Deleted");
+}
+console.log(!!10);
---
+++ UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/How Js Work Behind Scenes/thisKeyword.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+const jonas = {
+	name: "Rahul Rudra",
+
+	age: 34,
+
+	printOutput: function () {
+		console.log(`My Name is ${this.name}`);
+	},
+};
+
+console.log(jonas.printOutput());
