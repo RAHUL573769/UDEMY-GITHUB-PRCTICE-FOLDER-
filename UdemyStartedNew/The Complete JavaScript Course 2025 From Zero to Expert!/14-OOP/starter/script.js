342e923abffe14ac00e50add2cf0d94c6f5ccdff
Rahul Rudra <rahulrudra665@gmail.com>
2025-09-16 12:05:27 +0600

    changes made

--- UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/14-OOP/starter/script.js
+++ UdemyStartedNew/The Complete JavaScript Course 2025 From Zero to Expert!/14-OOP/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //instance properties
+  console.log(this);
+
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas instanceof Person);
+//Prototype
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+console.log(Person.prototype);
+
+jonas.calcAge();
+
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(jonas);
